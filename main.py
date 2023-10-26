37144f3df6d3eba4ecfe17b36bb43e0e283b8906
leo-2112 <148834379+leo-2112@users.noreply.github.com>
2023-10-26 17:50:35 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+#We initialize a variable for the player's name called pl_name with an empty string.
+pl_name = ""
+
+#We ask the user to input a name for the player and we print a welcome message
+print("Please enter the player's name: ")
+pl_name = input()
+print(f"Welcome {pl_name}!")
